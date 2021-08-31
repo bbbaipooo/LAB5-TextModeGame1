4d3663c49ac752acc0c43d0434d4fe0984857da3
bbbaipooo <baipoo_B@hotmail.com>
2021-08-31 15:04:02 +0700

    Add project files.

---
+++ lab5-textmodegame1/Source.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void draw_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf(" ..-%d-..  ", x);
+}
+
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf("         ");
+}
+
+int main()
+{
+	//gotoxy(38, 20);
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	char ch = ' ';
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a' && x > 0)
+			{
+				//erase_ship(x, y);
+				draw_ship(--x, y);
+			}
+			if (ch == 'd' && x < 69)
+			{
+				//erase_ship(x, y);
+				draw_ship(++x, y);
+			}
+			if (ch == 'w' && y > 0)
+			{
+				erase_ship(x, y);
+				draw_ship(x, --y);
+			}
+			if (ch == 's')
+			{
+				erase_ship(x, y);
+				draw_ship(x, ++y);
+			}
+			fflush(stdin);
+		}
+		Sleep(500);
+	} while (ch != 'x');
+	return 0;
+}
